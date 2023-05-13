9687a7c44ef699534fe32c8cc73da9948d826dcc
ferbarr <barrerarosalesfernando8@gmail.com>
2023-05-12 22:10:16 -0600

    registro visita

--- js/index.js
+++ js/index.js
@@ -2,14 +2,14 @@ const form = document.querySelector("#formulario"); //Seleccionar formulario y g
 const input = document.querySelector("#comentario"); //Seleccionar input y guardar sus valores en la constante input
 const boton = document.querySelector("#boton"); //Seleccionar boton y guardar sus valores en la constante boton
 
-form.addEventListener("submit", async (e) => {// Agregra evento al formulario de tipo submit que es para mandar datos
-  
+form.addEventListener("submit", async (e) => {
+  // Agregra evento al formulario de tipo submit que es para mandar datos
+
   e.preventDefault(); //Para evitar que se recargue la pagina que es lo que hace por defecto el evento submit
   const valor = input.value; //Obtenemos el valor escrito en el input y lo guardamos en la constante valor
-  if(!valor){
-    alert('Primero ingresa un comentario');
-    return
-
+  if (!valor) {
+    alert("Primero ingresa un comentario");
+    return;
   }
   try {
     boton.disabled = true; //Deshabilitamos el boton
@@ -29,8 +29,29 @@ form.addEventListener("submit", async (e) => {// Agregra evento al formulario de
   } catch (e) {
     //En caso de error lo muestra en la consola
     console.log(e);
-    alert('Ocurrió un error, vuelva a intentarlo')
+    alert("Ocurrió un error, vuelva a intentarlo");
   } finally {
     boton.disabled = false; //Habilitamos otra vez el boton
   }
 });
+
+const registrarVisita = async () => {
+  const fecha = new Date();
+  const fecha2 = fecha.toLocaleDateString("es-Es");
+
+  try {
+    await fetch(
+      //Realizar peticion
+      "https://curriculum-fer-default-rtdb.firebaseio.com/cv.json",
+      {
+        method: "POST", //Indicamos el metodo de la peticion
+        headers: {
+          "Content-Type": "application/json", //Indicamos el tipo de datos
+        },
+        body: JSON.stringify(fecha2), //Mandamos el valor del input en formato JSON
+      }
+    );
+  } catch (e) {
+    console.log(e);
+  }
+};
